4198a9dfdaa0a0a9636318a4ca77bbac6045f9f0
fgrunerme <fgruner@redhat.com>
2025-06-02 21:29:46 +0200

    fixed whereuser permissions

--- scripts/create-user.sql
+++ scripts/create-user.sql
@@ -10,5 +10,24 @@ $$;
 -- Grant connect permission to database
 GRANT CONNECT ON DATABASE whereisthisplace TO whereuser;
 
--- Grant usage on schema
-GRANT USAGE ON SCHEMA whereisthisplace TO whereuser;
+-- Grant usage and create permissions on public schema (not whereisthisplace schema)
+GRANT USAGE ON SCHEMA public TO whereuser;
+GRANT CREATE ON SCHEMA public TO whereuser;
+GRANT ALL PRIVILEGES ON SCHEMA public TO whereuser;
+
+-- Grant all privileges on the database
+GRANT ALL PRIVILEGES ON DATABASE whereisthisplace TO whereuser;
+
+-- Make whereuser a superuser (needed for creating extensions like PostGIS and pgvector)
+ALTER USER whereuser WITH SUPERUSER CREATEDB CREATEROLE;
+
+-- Set default privileges for future objects created by any user
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO whereuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO whereuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO whereuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TYPES TO whereuser;
+
+-- Grant privileges on existing objects (if any)
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO whereuser;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO whereuser;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO whereuser;
