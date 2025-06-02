43969c2c88e2fa0d2b915d65e117509b3a235e45
fgrunerme <fgruner@redhat.com>
2025-06-02 21:48:14 +0200

    retry permissions

--- scripts/create-user.sql
+++ scripts/create-user.sql
@@ -1,12 +1,11 @@
-# Replace the current script with proper permissions
 cat > scripts/create-user.sql << 'EOF'
--- Create application user
+-- Create application user with superuser privileges
 DO $$
 BEGIN
    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'whereuser') THEN
       CREATE USER whereuser WITH PASSWORD 'wherepass' SUPERUSER CREATEDB CREATEROLE;
    ELSE
-      ALTER USER whereuser WITH SUPERUSER CREATEDB CREATEROLE;
+      ALTER USER whereuser WITH PASSWORD 'wherepass' SUPERUSER CREATEDB CREATEROLE;
    END IF;
 END
 $$;
@@ -15,7 +14,7 @@ $$;
 GRANT CONNECT ON DATABASE whereisthisplace TO whereuser;
 GRANT ALL PRIVILEGES ON DATABASE whereisthisplace TO whereuser;
 
--- Grant permissions on BOTH schemas
+-- Grant permissions on BOTH schemas (public and whereisthisplace)
 GRANT USAGE ON SCHEMA public TO whereuser;
 GRANT CREATE ON SCHEMA public TO whereuser;
 GRANT ALL PRIVILEGES ON SCHEMA public TO whereuser;
@@ -24,12 +23,23 @@ GRANT USAGE ON SCHEMA whereisthisplace TO whereuser;
 GRANT CREATE ON SCHEMA whereisthisplace TO whereuser;
 GRANT ALL PRIVILEGES ON SCHEMA whereisthisplace TO whereuser;
 
--- Set default privileges for both schemas
+-- Set default privileges for future objects in both schemas
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO whereuser;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO whereuser;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO whereuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TYPES TO whereuser;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA whereisthisplace GRANT ALL ON TABLES TO whereuser;
 ALTER DEFAULT PRIVILEGES IN SCHEMA whereisthisplace GRANT ALL ON SEQUENCES TO whereuser;
 ALTER DEFAULT PRIVILEGES IN SCHEMA whereisthisplace GRANT ALL ON FUNCTIONS TO whereuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA whereisthisplace GRANT ALL ON TYPES TO whereuser;
+
+-- Grant privileges on existing objects in both schemas
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO whereuser;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO whereuser;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO whereuser;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA whereisthisplace TO whereuser;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA whereisthisplace TO whereuser;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA whereisthisplace TO whereuser;
 EOF
